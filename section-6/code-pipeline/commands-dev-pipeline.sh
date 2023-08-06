5ebccc3debb7e4e9e2f981fe2714e738457ffd0e
airflow-workstation <daniel@mgma.io>
2023-08-05 23:47:14 -0600

    changes for local

--- section-6/code-pipeline/commands-dev-pipeline.sh
+++ section-6/code-pipeline/commands-dev-pipeline.sh
@@ -2,13 +2,13 @@
 aws s3 mb s3://airflow-dev-codepipeline-artifacts
 
 # Create IAM roles and add iniline policies so that CodePipeline can interact with EKS through kubectl
-aws iam create-role --role-name AirflowCodePipelineServiceRole --assume-role-policy-document file://airflow-materials-aws/section-6/code-pipeline/cpAssumeRolePolicyDocument.json
-aws iam put-role-policy --role-name AirflowCodePipelineServiceRole --policy-name codepipeline-access --policy-document file://airflow-materials-aws/section-6/code-pipeline/cpPolicyDocument.json
-aws iam create-role --role-name AirflowCodeBuildServiceRole --assume-role-policy-document file://airflow-materials-aws/section-6/code-pipeline/cbAssumeRolePolicyDocument.json
-aws iam put-role-policy --role-name AirflowCodeBuildServiceRole --policy-name codebuild-access --policy-document file://airflow-materials-aws/section-6/code-pipeline/cbPolicyDocument.json
+aws iam create-role --role-name AirflowCodePipelineServiceRole --assume-role-policy-document file:///Users/dani/airflow-workplace/airflow-materials-aws/section-6/code-pipeline/cpAssumeRolePolicyDocument.json
+aws iam put-role-policy --role-name AirflowCodePipelineServiceRole --policy-name codepipeline-access --policy-document file:///Users/dani/airflow-workplace/airflow-materials-aws/section-6/code-pipeline/cpPolicyDocument.json
+aws iam create-role --role-name AirflowCodeBuildServiceRole --assume-role-policy-document file:///Users/dani/airflow-workplace/airflow-materials-aws/section-6/code-pipeline/cbAssumeRolePolicyDocument.json
+aws iam put-role-policy --role-name AirflowCodeBuildServiceRole --policy-name codebuild-access --policy-document file:///Users/dani/airflow-workplace/airflow-materials-aws/section-6/code-pipeline/cbPolicyDocument.json
 
 # open the flux pipeline to set the bucket name you created under "ArtifactStore"
 vim airflow-materials-aws/code-pipeline/airflow-dev-pipeline.cfn.yml
 
 # Create the AWS CodePipeline using CloudFormation (This doesn't deploy the image as Flux handles it)
-aws cloudformation create-stack --stack-name=airflow-dev-pipeline --template-body=file://airflow-materials-aws/section-6/code-pipeline/airflow-dev-pipeline.cfn.yml --parameters ParameterKey=GitHubUser,ParameterValue=marclamberti ParameterKey=GitHubToken,ParameterValue=cb53803446b0968e132e2e8ff729c7596fb0d7c8 ParameterKey=GitSourceRepo,ParameterValue=airflow-eks-docker ParameterKey=GitBranch,ParameterValue=dev
+aws cloudformation create-stack --stack-name=airflow-dev-pipeline --template-body=file:///Users/dani/airflow-workplace/airflow-materials-aws/section-6/code-pipeline/airflow-dev-pipeline.cfn.yml --parameters ParameterKey=GitHubUser,ParameterValue=Mgmaplus ParameterKey=GitHubToken,ParameterValue=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 ParameterKey=GitSourceRepo,ParameterValue=airflow-eks-docker ParameterKey=GitBranch,ParameterValue=dev
